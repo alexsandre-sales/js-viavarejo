499efe44aff32e4cf71dffe60165ea7f874e1214
alexsandre-sales <lexkogake@gmail.com>
2022-07-12 23:16:20 -0300

    subindo

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,125 @@
-const mask = {
-  money(value) {
-    const cleanValue = +value.replace(/\D+/g, '');
-    const options = { style: 'currency', currency: 'BRL' };
-    return new Intl.NumberFormat('pt-br', options).format(cleanValue / 100);
-  }
-};
+let limpaDados = document.querySelector('.limpa_dados');
+let formMercadoria = document.querySelector('#formMercadoria');
+let menuMobile = document.querySelector('.menu-mobile');
+let btnMenu = document.querySelector('.img-menu');
 let valor = document.querySelector('#valor');
+let items = JSON.parse(localStorage.getItem('items')) || [];
+
 valor.addEventListener('input', element => {
-  element.target.value = mask.money(element.target.value);
+  element.target.value = valorMascara(element.target.value);
 });
+
+formMercadoria.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const tipoTransacao = event.target.elements['type'];
+  const nomeMercadoria = event.target.elements['nomeMercadoria'];
+  const valor = event.target.elements['valor'];
+
+  items.push({
+    tipoTransacao: tipoTransacao.value,
+    nomeMercadoria: nomeMercadoria.value,
+    valor: valor.value
+  });
+
+  criaLista();
+  localStorage.setItem('items', JSON.stringify(items));
+  nomeMercadoria.value = '';
+  valor.value = '';
+});
+
+limpaDados.addEventListener('click', () => {
+  let text = 'Deseja limpar os dados ?';
+
+  if (items.length > 0 && confirm(text) == true) {
+    localStorage.removeItem('items');
+    items = [];
+    criaLista();
+  } else if (items.length <= 0) {
+    alert('Não tem dados para apagar');
+  }
+});
+
+btnMenu.addEventListener('click', () => {
+  menuMobile.classList.add('ativo');
+});
+
+menuMobile.addEventListener('click', () => {
+  menuMobile.classList.remove('ativo');
+});
+
+function criaLista() {
+  let tBodylista = document.querySelector('.listaLinha');
+  let tFoot = document.querySelector('.footer');
+  tBodylista.innerHTML = '';
+  tFoot.innerHTML = '';
+  let total = 0;
+  let soma = 0;
+  for (const item of items) {
+    let tr = tBodylista.insertRow();
+    let tdColuna1 = tr.insertCell();
+    let tdColuna2 = tr.insertCell();
+    let tdColuna3 = tr.insertCell();
+    tdColuna2.classList.add('positive');
+    tdColuna3.innerHTML = item.valor;
+
+    soma = item.valor.replace(/[^0-9,]*/g, '').replace(',', '.');
+    if (item.tipoTransacao == 'compra') {
+      total -= parseFloat(soma);
+      tdColuna2.innerHTML = ` -<span>${item.nomeMercadoria}</span>`;
+    } else {
+      total += parseFloat(soma);
+      tdColuna2.innerHTML = ` +<span>${item.nomeMercadoria}</span>`;
+    }
+  }
+
+  if (items.length == 0) {
+    let semConteudoTable = document.querySelector('.listaLinha');
+    semConteudoTable.innerHTML = `
+        <tr>
+          <td></td>
+          <td colspan='3'>
+            <p class='noConteudo'>
+              <span>Nenhuma transação cadastrada</span>
+            </p>
+          </td>
+        </tr>
+    `;
+  } else {
+    let footer = tFoot.insertRow();
+    let tdFooterColuna1 = footer.insertCell();
+    let tdFooterColuna2 = footer.insertCell();
+    let tdFooterColuna3 = footer.insertCell();
+    footer.classList.add('tr-total');
+    tdFooterColuna2.classList.add('space');
+    tdFooterColuna3.classList.add('profit');
+
+    let totalMask =
+      'R$ ' +
+      parseFloat(total)
+        .toFixed(2)
+        .replace('.', ',')
+        .replace(/([0-9]*)([0-9]{3},*)/, '$1.$2');
+
+    tdFooterColuna2.innerHTML = 'Total';
+
+    if (total === 0) {
+      tdFooterColuna3.innerHTML = valorMascara(totalMask) + '<span></span>';
+    } else if (total > 0) {
+      tdFooterColuna3.innerHTML =
+        valorMascara(totalMask) + '<span>[Lucro]</span>';
+    } else {
+      tdFooterColuna3.innerHTML =
+        valorMascara(totalMask) + '<span>[Prejuizo]</span>';
+    }
+  }
+}
+
+function valorMascara(value) {
+  const valorString = value.toString();
+  const cleanValue = +valorString.replace(/\D+/g, '');
+  const options = { style: 'currency', currency: 'BRL' };
+  return new Intl.NumberFormat('pt-br', options).format(cleanValue / 100);
+}
+
+criaLista();
--- readme.md
+++ readme.md
@@ -1,26 +1,50 @@
-// let tipoTransacao = document.querySelector('#type').value;
-// let nomeMercadoria = document.querySelector('#nomeMercadoria').value;
-// let valor = document.querySelector('#valor').value;
-let button = document.querySelector('.button');
+# O desafio
 
-let formMercadoria = document.querySelector('#formMercadoria');
-let items = JSON.parse(localStorage.getItem('items')) || [];
+Seu objetivo é criar uma SPA (Single Page Application) seguindo o layout que está em esta url https://goo.gl/yMrCaf.
 
-\***\*\*\*\***pt2
-formMercadoria.addEventListener('submit', event => {
-event.preventDefault();
+## O que sua aplicação deverá fazer
 
-const tipoTransacao = event.target.elements['type'];
-const nomeMercadoria = event.target.elements['nomeMercadoria'];
-const valor = event.target.elements['valor'];
+#
 
-items.push({
-tipoTransacao: tipoTransacao.value,
-nomeMercadoria: nomeMercadoria.value,
-valor: valor.value
-});
+- Incluir transações de compra ou venda de mercadoria
+- Persistir as transações no local storage
+- Criar um extrato das transações incluídas. As transações deverão ser mostradas na ordem em que foram incluídas
+- Mostrar o saldo final e destacar se houve lucro ou prejuizo
+- A aplicação deverá ser responsiva e estar de acordo com o layout fornecido.
 
-//localStorage.setItem('items', JSON.stringify(items));
-//nomeMercadoria.value = '';
-//valor.value = '';
-//});
+## Sobre o layout
+
+#
+
+O layout fornecido não é muito complexo visualmente, nosso maior interesse é em ver como você reutiliza código e resolve algumas questões apresentadas nele
+
+- O menu principal do cabeçalho é apenas para efeito de layout, não há necessidade de desenvolver outras páginas;
+
+- O layout deve se adaptar e mudar de acordo com o tamanho da tela
+
+  - Testaremos em smartphones, tablets (modos portrait e landscape) e monitores a partir de 1024px até 1900px
+
+- A fonte utilizada é a Lato
+
+- A largura máxima do conteúdo é 1100px
+
+#
+
+## O que será avaliado
+
+- HTML semântico, limpo e claro
+
+- CSS responsivo, semântico, reutilizável e seguindo boas práticas
+
+- Conhecimento de Javascript orientado a objeto, funcional e/ou reativo
+- Utilização correta de git
+- O código deverá funcionar no Chrome
+
+Tecnologias que você pode utilizar
+Você é livre para utilizar qualquer framework JS que preferir, mas saiba que aqui na Via Varejo por enquanto trabalhamos com Angular 4 e 7
+
+Na parte de CSS, você é livre de utilizar qualquer preprocessador também, mas não é permitido utilizar nenhum framework CSS. Aqui na Via Varejo utilizamos Sass (scss).
+
+#
+
+...utilizado JS puro
